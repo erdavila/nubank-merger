c61840d2cb289478c688d877cb37a707acbe99f2
Eduardo R. D'Avila <erdavila@gmail.com>
2018-04-02 19:43:28 -0300

    run.sh: Check arguments

--- run.sh
+++ run.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-scala target/scala-2.12/nubank-merger_2.12-0.0.0.1.jar "$@"
+if [[ -z $2 ]] ; then
+	echo "Use arguments: $0 INVOICE HISTORY"
+	exit 1
+fi
+
+INVOICE=$(cygpath -w $1)
+HISTORY=$(cygpath -w $2)
+
+# Command to generate the JAR file:
+#   $ sbt package
+#
+scala target/scala-2.12/nubank-merger_2.12-0.0.0.1.jar $INVOICE $HISTORY
+
+#sbt "run $INVOICE $HISTORY"
